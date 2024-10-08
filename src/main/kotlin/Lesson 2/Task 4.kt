2456cdaf0fed1af0a8ab8877f63f631854cb4d6f
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-08 12:44:15 +0300

    KS 2-4-The first commit

---
+++ src/main/kotlin/Lesson 2/Task 4.kt
@@ -0,0 +1,10 @@
+package org.example.`Lesson 2`
+
+fun main() {
+    val cristalOre = 7
+    val ironOre = 11
+    val baffCristalOre = (cristalOre * 0.2).toInt()
+    val baffIronOre = (ironOre * 0.2).toInt()
+    println(baffCristalOre)
+    println(baffIronOre)
+}
