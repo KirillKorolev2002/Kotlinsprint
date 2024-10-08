b341d2454a46b221c164eed2258a3f029c07688d
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-08 11:15:55 +0300

    KS 2-2-The first commit

---
+++ src/main/kotlin/Lesson 2/Task 2.kt
@@ -0,0 +1,15 @@
+package org.example.`Lesson 2`
+
+fun main () {
+    val countOfWorkers=50
+    val workersSalary=30000
+    val countOfTrainees=30
+    val traineesSalary=20000
+    val expencesOnWorkersSalary=countOfWorkers*workersSalary
+    println(expencesOnWorkersSalary)
+    val expencesOnTraineesSalary=countOfTrainees*traineesSalary
+    val summonCountOfSalaries= expencesOnWorkersSalary+expencesOnTraineesSalary
+    println(summonCountOfSalaries)
+    val averageSalaryOfOneWorker= summonCountOfSalaries/80
+    println(averageSalaryOfOneWorker)
+}
