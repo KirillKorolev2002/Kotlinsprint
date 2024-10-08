7654c6ade56bee0c196470f2dd9e96ba61e2e3e6
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-08 10:47:17 +0300

    KS 2-1-The first commit

--- src/main/kotlin/lesson_2/Task_1.kt
+++ src/main/kotlin/lesson_2/Task_1.kt
@@ -1,10 +1,10 @@
 package org.example.lesson2
 
 fun main() {
-val firstPupilMark=3
-    val secondPupilMark=4
-      val thirdPupilMark=3
-        val fourthPupilMark=5
-    val summOfMarks=(firstPupilMark+secondPupilMark+thirdPupilMark+fourthPupilMark)/4f
+    val firstPupilMark = 3
+    val secondPupilMark = 4
+    val thirdPupilMark = 3
+    val fourthPupilMark = 5
+    val summOfMarks = (firstPupilMark + secondPupilMark + thirdPupilMark + fourthPupilMark) / 4f
     println(summOfMarks)
 }
