38f755718422290a3e355ff283c512bee7d60431
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-09 16:30:11 +0300

    KS 3-1-The First commit

---
+++ src/main/kotlin/Lesson3/Task1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson3
+
+fun main() {
+    val nameOfUser = readln()
+    val theDayTime = "Добрый день,$nameOfUser!"
+    val theEveningTime = "Добрый вечер,$nameOfUser!"
+    println(theDayTime)
+    println(theEveningTime)
+}
