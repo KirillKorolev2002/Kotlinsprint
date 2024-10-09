111c4c230c0e7c5d04a95fa6a0bb0a518813391c
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-09 17:31:20 +0300

    KS 3-2-The first commit

---
+++ src/main/kotlin/Lesson 3/Task 2.kt
@@ -0,0 +1,12 @@
+package org.example.`Lesson 3`
+
+fun main() {
+    val theSurNameOfWomen = "Андреева Татьяна Сергеевна"
+    val theAgeOfWomen = "20"
+    val theSecondLastSurName = "Сидорова"
+    val currentAge = "22"
+    val theNameOfWomenAfterMarriage = "$theSecondLastSurName Татьяна Сергеевна"
+
+    println(theSurNameOfWomen + "," + theAgeOfWomen)
+    println(theNameOfWomenAfterMarriage + "," + currentAge)
+}
