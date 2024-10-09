00846578e8f5595d6e04135b157b17114df70cf3
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-09 15:01:01 +0300

    KS 2-4-add variable

--- src/main/kotlin/Lesson 2/Task 4.kt
+++ src/main/kotlin/Lesson 2/Task 4.kt
@@ -3,8 +3,9 @@ package org.example.`Lesson 2`
 fun main() {
     val cristalOre = 7
     val ironOre = 11
-    val baffCristalOre = (cristalOre * 0.2).toInt()
-    val baffIronOre = (ironOre * 0.2).toInt()
+    val baffpercentages = 0.2
+    val baffCristalOre = (cristalOre * baffpercentages).toInt()
+    val baffIronOre = (ironOre * baffpercentages).toInt()
     println(baffCristalOre)
     println(baffIronOre)
 }
