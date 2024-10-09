d719dcc9d89994a3c6476850f68e7082b6745235
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-09 19:06:17 +0300

    KS 3-4

---
+++ src/main/kotlin/Lesson 3/Task 4.kt
@@ -0,0 +1,14 @@
+package org.example.`Lesson 3`
+
+fun main() {
+    val theStepFrom = "E2"
+    val theStepOn = "E4"
+    val theNumberOfStep = 1
+    val sentOnServer = "$theStepFrom-$theStepOn;$theNumberOfStep"
+    println(sentOnServer)
+    val theNewStepFrom = "D2"
+    val theNewStepOn = "D3"
+    val theNewNumberOfStep = 2
+    val newSentOnServer = "$theNewStepFrom-$theNewStepOn;$theNewNumberOfStep"
+    println(newSentOnServer)
+}
