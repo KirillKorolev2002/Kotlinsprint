e55a4f929aeb7ec9ae0569bc22133e053f013eb2
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-09 14:51:42 +0300

    KS 2-3-use String.Format

--- src/main/kotlin/Lesson 2/Task 3.kt
+++ src/main/kotlin/Lesson 2/Task 3.kt
@@ -1,16 +1,17 @@
 package org.example.`Lesson 2`
 
 fun main() {
-    val timeOfHoursOfDeparture = readln().toInt()
-    val timeOfMinutesOfDeparture = readln().toInt()
-    val timeOfWay = readln().toInt()
-    val timeInWholeHours = timeOfWay / 60
-    val timeInWholeMinutes = timeInWholeHours * 60
+    val timeOfHoursOfDeparture = 9
+    val timeOfMinutesOfDeparture = 39
+    val timeOfWay = 457
+    val hoursinMinutes=60
+    val timeInWholeHours = timeOfWay / hoursinMinutes
+    val timeInWholeMinutes = timeInWholeHours * hoursinMinutes
     val remainingTimeInMinutes = timeOfWay - timeInWholeMinutes
     val timeOfArrivalInHours = timeOfHoursOfDeparture + timeInWholeHours
     val timeOfArrivalInRemainingHours = (timeOfMinutesOfDeparture + remainingTimeInMinutes) / 60
-    val timeOfArrivalInMinutes = timeOfArrivalInRemainingHours * 60
+    val timeOfArrivalInMinutes = timeOfArrivalInRemainingHours * hoursinMinutes
     val timeOfArrivalInRemainingMinutes = (timeOfMinutesOfDeparture + remainingTimeInMinutes) - timeOfArrivalInMinutes
     val timeOfArrivalInHoursFinal = timeOfArrivalInHours + timeOfArrivalInRemainingHours
-    println("time is " + timeOfArrivalInHoursFinal + ":" + timeOfArrivalInRemainingMinutes)
+    println(String.format("%d:%d",timeOfArrivalInHoursFinal,timeOfArrivalInRemainingMinutes))
 }
