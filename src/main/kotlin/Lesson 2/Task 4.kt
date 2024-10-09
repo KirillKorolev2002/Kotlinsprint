2b534894fa2a00730bada16ff5b7a2dc58600f84
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-09 16:49:02 +0300

    KS 2-4-Correction

--- src/main/kotlin/Lesson 2/Task 4.kt
+++ src/main/kotlin/Lesson 2/Task 4.kt
@@ -4,8 +4,8 @@ fun main() {
     val cristalOre = 7
     val ironOre = 11
     val baffPercentages = 20
-    val baffCristalOre = (cristalOre * (baffPercentages/100))
-    val baffIronOre = (ironOre * (baffPercentages/100))
+    val baffCristalOre = (cristalOre * (baffPercentages / 100))
+    val baffIronOre = (ironOre * (baffPercentages / 100))
     println(baffCristalOre)
     println(baffIronOre)
 }
