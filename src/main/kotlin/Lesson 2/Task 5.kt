9a7c86c223c315a1ad9cad493823808e85022eac
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-09 15:13:25 +0300

    KS 2-5-add variables

--- src/main/kotlin/Lesson 2/Task 5.kt
+++ src/main/kotlin/Lesson 2/Task 5.kt
@@ -5,7 +5,7 @@ import kotlin.math.pow
 fun main() {
     val currentCost = 70000
     val interestRate = 16.7
-    val periodOfInvest=20
-    val futureCost = currentCost * (1 + interestRate/100).pow(periodOfInvest)
+    val periodOfInvest = 20
+    val futureCost = currentCost * (1 + interestRate / 100).pow(periodOfInvest)
     print(String.format("%.3f", futureCost))
 }
