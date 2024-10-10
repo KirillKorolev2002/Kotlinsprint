02fae6ce4b29a725872793258bc141ba7c4614c6
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-10 22:06:15 +0300

    KS 3-5

--- src/main/kotlin/Lesson 3/Task 5.kt
+++ src/main/kotlin/Lesson 3/Task 5.kt
@@ -4,16 +4,21 @@ fun main() {
     val theStepFrom = "D2"
     val theStepOn = "D4"
     val theNumberOfStep = 0
+
     val sentOnServer = "$theStepFrom-$theStepOn;$theNumberOfStep"
+
     println(sentOnServer)
+
     val theNewStepFrom = "D2"
     val theNewStepOn = "D4"
     val theNewNumberOfStep = 0
+
     val NewsentOnServer = """
         $theNewStepFrom
         $theNewStepOn
         $theNewNumberOfStep
     """.trimIndent()
+
     println(NewsentOnServer)
 
 }
