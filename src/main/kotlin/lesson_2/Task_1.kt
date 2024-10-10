5142152399d3f7c1d1a60cc2d42f98f16f95284f
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-10 16:23:39 +0300

    KS 2-1 Change toFloat to tgesummOfMarks

--- src/main/kotlin/lesson_2/Task_1.kt
+++ src/main/kotlin/lesson_2/Task_1.kt
@@ -1,11 +1,15 @@
 package org.example.lesson2
 
 fun main() {
+
     val numberOfPupils = 4
     val firstPupilMark = 3
     val secondPupilMark = 4
     val thirdPupilMark = 3
     val fourthPupilMark = 5
-    val summOfMarks = (firstPupilMark + secondPupilMark + thirdPupilMark + fourthPupilMark) / numberOfPupils
-    println(summOfMarks.toFloat())
+
+    val summOfMarks = (firstPupilMark + secondPupilMark + thirdPupilMark + fourthPupilMark).toFloat()
+    val averageSummOfMarks = (summOfMarks / numberOfPupils)
+
+    println(averageSummOfMarks)
 }
