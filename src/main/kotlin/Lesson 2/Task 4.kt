aa9936dde36b4d03f1805ca855f0c093be26ae9e
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-10 16:28:26 +0300

    KS 2-4 Get rid of magical words

--- src/main/kotlin/Lesson 2/Task 4.kt
+++ src/main/kotlin/Lesson 2/Task 4.kt
@@ -1,11 +1,15 @@
 package org.example.`Lesson 2`
 
 fun main() {
+
     val cristalOre = 7
     val ironOre = 11
     val baffPercentages = 20
-    val baffCristalOre = (cristalOre * (baffPercentages / 100))
-    val baffIronOre = (ironOre * (baffPercentages / 100))
+    val transferToPercentages = 100
+
+    val baffCristalOre = (cristalOre * (baffPercentages / transferToPercentages))
+    val baffIronOre = (ironOre * (baffPercentages / transferToPercentages))
+
     println(baffCristalOre)
     println(baffIronOre)
 }
