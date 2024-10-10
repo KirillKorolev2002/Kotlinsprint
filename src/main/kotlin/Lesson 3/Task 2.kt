47fd56c8a8630cd2bbe8aa6665fb41905198291b
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-10 15:06:18 +0300

    KS 3-2 Changes

--- src/main/kotlin/Lesson 3/Task 2.kt
+++ src/main/kotlin/Lesson 3/Task 2.kt
@@ -1,12 +1,13 @@
 package org.example.`Lesson 3`
 
 fun main() {
-    val theSurNameOfWomen = "Андреева Татьяна Сергеевна"
-    val theAgeOfWomen = "20"
-    val theSecondLastSurName = "Сидорова"
-    val currentAge = "22"
-    val theNameOfWomenAfterMarriage = "$theSecondLastSurName Татьяна Сергеевна"
+    val theFirstName = "Татьяна"
+    val thePatronymic = "Сергеевна"
+    val theSurNameOfWomen = "Андреева"
+    val theAgeOfWomenInThePast = "20"
+    val theCurrentLastSurName = "Сидорова"
+    val theCurrentAge = "22"
 
-    println(theSurNameOfWomen + "," + theAgeOfWomen)
-    println(theNameOfWomenAfterMarriage + "," + currentAge)
+    println(theSurNameOfWomen + " " + theFirstName + " " + thePatronymic + "," + theAgeOfWomenInThePast)
+    println(theCurrentLastSurName + " " + theFirstName + " " + thePatronymic + "," + theCurrentAge)
 }
