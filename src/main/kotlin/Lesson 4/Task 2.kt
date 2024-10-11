b451d4d802e8afa4c9f856526b8cf0c13c714d83
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-11 23:00:54 +0300

    KS 4-2 TheFirstCommit

--- src/main/kotlin/Lesson 4/Task 2.kt
+++ src/main/kotlin/Lesson 4/Task 2.kt
@@ -1,12 +1,12 @@
 package org.example.`Lesson 4`
 
-fun main () {
-    val theMinimumWeightOfCargo=35
-    val theMaximumWeightOfCargo=100
-    val theMaximumVolumeOfCargo=100
-    val theWeightCargo=readln().toInt()
-    val theVolumeCargo=readln().toInt()
+fun main() {
+    val theMinimumWeightOfCargo = 35
+    val theMaximumWeightOfCargo = 100
+    val theMaximumVolumeOfCargo = 100
+    val theWeightCargo = readln().toInt()
+    val theVolumeCargo = readln().toInt()
 
 
-    println("Груз с весом $theWeightCargo кг и объемом $theVolumeCargo л соответствует категории 'Average': ${ (theWeightCargo <= theMaximumWeightOfCargo && theMinimumWeightOfCargo < theWeightCargo) and (theVolumeCargo < theMaximumVolumeOfCargo)}")
+    println("Груз с весом $theWeightCargo кг и объемом $theVolumeCargo л соответствует категории 'Average': ${(theWeightCargo <= theMaximumWeightOfCargo && theMinimumWeightOfCargo < theWeightCargo) and (theVolumeCargo < theMaximumVolumeOfCargo)}")
 }
