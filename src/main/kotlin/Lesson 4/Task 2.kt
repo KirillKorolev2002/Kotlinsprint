4abfc8421e514ab7da08ce4b2c1a7eda3f8eee70
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-12 23:02:27 +0300

    KS 4-2 Сhanges-1

--- src/main/kotlin/Lesson 4/Task 2.kt
+++ src/main/kotlin/Lesson 4/Task 2.kt
@@ -1,12 +1,13 @@
 package org.example.`Lesson 4`
 
 fun main() {
-    val theMinimumWeightOfCargo = 35
-    val theMaximumWeightOfCargo = 100
-    val theMaximumVolumeOfCargo = 100
     val theWeightCargo = readln().toInt()
     val theVolumeCargo = readln().toInt()
-
-
-    println("Груз с весом $theWeightCargo кг и объемом $theVolumeCargo л соответствует категории 'Average': ${(theWeightCargo <= theMaximumWeightOfCargo && theMinimumWeightOfCargo < theWeightCargo) and (theVolumeCargo < theMaximumVolumeOfCargo)}")
+    val conditionOne = theWeightCargo <= THE_MAXIMUM_WEIGHT_OF_CARGO && THE_MINIMUM_WEIGHT_OF_CARGO < theWeightCargo
+    val conditionTwo = theVolumeCargo < THE_MAXIMUM_VOLUME_OF_CARGO
+    println("Груз с весом $theWeightCargo кг и объемом $theVolumeCargo л соответствует категории 'Average': ${(conditionOne) and (conditionTwo)}")
 }
+
+val THE_MINIMUM_WEIGHT_OF_CARGO = 35
+val THE_MAXIMUM_WEIGHT_OF_CARGO = 100
+val THE_MAXIMUM_VOLUME_OF_CARGO = 100
