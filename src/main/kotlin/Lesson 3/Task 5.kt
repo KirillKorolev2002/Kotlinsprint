501eef13a64719d0d0a0aa51fe8196e2c8c1b2fb
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-12 10:19:02 +0300

    KS 3-5 Changes-1

--- src/main/kotlin/Lesson 3/Task 5.kt
+++ src/main/kotlin/Lesson 3/Task 5.kt
@@ -4,16 +4,16 @@ fun main() {
 
     val sentOnServer = "D2-D4;0"
 
-    val theNewStepFrom = "D2"
-    val theNewStepOn = "D4"
-    val theNewNumberOfStep = 0
+    val theDivideString = sentOnServer.split("-", ";")
+    println(theDivideString)
+    // Я не понял как обратиться к элементам строки, не как к массиву символов
+    val fromPosition = theDivideString[0]
+    val toPosition = theDivideString[1]
+    val moveNumber = theDivideString[2]
 
-    val NewsentOnServer = """
-        $theNewStepFrom
-        $theNewStepOn
-        $theNewNumberOfStep
-    """.trimIndent()
+    println("$fromPosition")
+    println("$toPosition")
+    println("$moveNumber")
 
-    println(NewsentOnServer)
 
 }
