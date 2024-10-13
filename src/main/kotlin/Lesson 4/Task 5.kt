b0b63388585c507fd3367e928460c030a901f82d
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-13 15:48:50 +0300

    KS 4-5 add Boolean

--- src/main/kotlin/Lesson 4/Task 5.kt
+++ src/main/kotlin/Lesson 4/Task 5.kt
@@ -3,25 +3,26 @@ package org.example.`Lesson 4`
 fun main() {
 
     val theDamage = readln()
-    val hasDamage = "Имеет повреждения корпуса"
-    val noDamage = "Не имеет повреждений корпуса"
-    val haveDamagesToday = (theDamage != hasDamage) and (theDamage == noDamage)
+    val isDamaged = "Имеет повреждения корпуса"
+    val isNoDamaged = "Не имеет повреждений корпуса"
+    val haveDamagesToday = (theDamage != isDamaged) and (theDamage == isNoDamaged)
 
     val crewCount = readln().toInt()
-    val theMinimumOfCrew = 55
-    val theMaximumOfCrew = 70
-    val theMinimumOfProvision = 50
-    val crewCountCanBeOnTheShip = (theMinimumOfCrew <= crewCount) and (crewCount <= theMaximumOfCrew)
+    val crewCountCanBeOnTheShip = (THE_MINIMUM_OF_CREW <= crewCount) and (crewCount <= THE_MAXIMUM_OF_CREW)
 
     val provisionBoxes = readln().toInt()
 
-    val theWeather = readln()
-    val badWeather = "Неблагоприятная"
-    val goodWeather = "Благоприятная"
-    val theWeatherOnBoat = (theWeather != badWeather) and (theWeather == goodWeather)
+    val isTheWeather = readln()
+    val isBadWeather = "Неблагоприятная"
+    val isGoodWeather = "Благоприятная"
+    val theWeatherOnBoat = (isTheWeather != isBadWeather) and (isTheWeather == isGoodWeather)
 
-    val theSail = (haveDamagesToday && crewCountCanBeOnTheShip && provisionBoxes > theMinimumOfProvision) and
-            (crewCount == theMaximumOfCrew && theWeatherOnBoat && provisionBoxes >= theMinimumOfProvision)
+    val theSail = (haveDamagesToday && crewCountCanBeOnTheShip && provisionBoxes > THE_MINIMUM_OF_PROVISION) and
+            (crewCount == THE_MINIMUM_OF_CREW && theWeatherOnBoat && provisionBoxes >= THE_MINIMUM_OF_PROVISION)
 
     println(theSail)
 }
+
+const val THE_MINIMUM_OF_CREW = 55
+const val THE_MAXIMUM_OF_CREW = 70
+const val THE_MINIMUM_OF_PROVISION = 50
