fa6f4cbe73b8df228c7aa5bf79e154e3bf64b2e1
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-13 18:12:29 +0300

    KS 5-3 Lottery Application

---
+++ src/main/kotlin/Lesson 5/Task 3.kt
@@ -0,0 +1,24 @@
+package org.example.`Lesson 5`
+
+fun main() {
+
+    println("Ввести первое число:")
+    val userNumber1 = readln().toInt()
+    println("Ввести второе число:")
+    val userNumber2 = readln().toInt()
+
+    if (userNumber1 == WINNING_NUMBER1 && WINNING_NUMBER2 == userNumber2) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (userNumber1 == WINNING_NUMBER1 && WINNING_NUMBER2 != userNumber2) {
+        println("Вы выиграли утешительный приз!")
+    } else if (userNumber1 != WINNING_NUMBER1 && WINNING_NUMBER2 == userNumber2) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача!")
+    }
+
+    println("Правильные числа для победы: $WINNING_NUMBER1 и $WINNING_NUMBER2")
+}
+
+const val WINNING_NUMBER1 = 8
+const val WINNING_NUMBER2 = 8
