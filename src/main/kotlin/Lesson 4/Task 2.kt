f0f81c6e6c881c99cc0676ec85fc3f3ebf6ccd5f
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-13 15:10:25 +0300

    KS 4-3 Change type on Boolean

--- src/main/kotlin/Lesson 4/Task 2.kt
+++ src/main/kotlin/Lesson 4/Task 2.kt
@@ -3,9 +3,9 @@ package org.example.`Lesson 4`
 fun main() {
     val theWeightCargo = readln().toInt()
     val theVolumeCargo = readln().toInt()
-    val conditionOne = theWeightCargo <= THE_MAXIMUM_WEIGHT_OF_CARGO && THE_MINIMUM_WEIGHT_OF_CARGO < theWeightCargo
-    val conditionTwo = theVolumeCargo < THE_MAXIMUM_VOLUME_OF_CARGO
-    println("Груз с весом $theWeightCargo кг и объемом $theVolumeCargo л соответствует категории 'Average': ${(conditionOne) and (conditionTwo)}")
+    val isConditionOneRespondAverage = theWeightCargo <= THE_MAXIMUM_WEIGHT_OF_CARGO && THE_MINIMUM_WEIGHT_OF_CARGO < theWeightCargo
+    val isConditionTwoRespondAverage = theVolumeCargo < THE_MAXIMUM_VOLUME_OF_CARGO
+    println("Груз с весом $theWeightCargo кг и объемом $theVolumeCargo л соответствует категории 'Average': ${(isConditionOneRespondAverage) && (isConditionTwoRespondAverage)}")
 }
 
 val THE_MINIMUM_WEIGHT_OF_CARGO = 35
