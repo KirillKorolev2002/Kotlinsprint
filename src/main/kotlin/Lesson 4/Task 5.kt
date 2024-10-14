a8a4430bdcc8651c2698393ebf1f0e17b311fcbd
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-14 17:38:53 +0300

    KS 4-5 TheShipChanges

--- src/main/kotlin/Lesson 4/Task 5.kt
+++ src/main/kotlin/Lesson 4/Task 5.kt
@@ -2,23 +2,17 @@ package org.example.`Lesson 4`
 
 fun main() {
 
-    val theDamage = readln()
-    val isDamaged = "Имеет повреждения корпуса"
-    val isNoDamaged = "Не имеет повреждений корпуса"
-    val haveDamagesToday = (theDamage != isDamaged) and (theDamage == isNoDamaged)
+    val isDamagesToday = IS_NOT_DAMAGED
 
     val crewCount = readln().toInt()
     val crewCountCanBeOnTheShip = (THE_MINIMUM_OF_CREW <= crewCount) and (crewCount <= THE_MAXIMUM_OF_CREW)
 
     val provisionBoxes = readln().toInt()
 
-    val isTheWeather = readln()
-    val isBadWeather = "Неблагоприятная"
-    val isGoodWeather = "Благоприятная"
-    val theWeatherOnBoat = (isTheWeather != isBadWeather) and (isTheWeather == isGoodWeather)
+    val isWeatherOnBoat = IS_GOOD_WEATHER
 
-    val theSail = (haveDamagesToday && crewCountCanBeOnTheShip && provisionBoxes > THE_MINIMUM_OF_PROVISION) and
-            (crewCount == THE_MINIMUM_OF_CREW && theWeatherOnBoat && provisionBoxes >= THE_MINIMUM_OF_PROVISION)
+    val theSail = (isDamagesToday && crewCountCanBeOnTheShip && provisionBoxes > THE_MINIMUM_OF_PROVISION) ||
+            (crewCount <= THE_MINIMUM_OF_CREW && isWeatherOnBoat && provisionBoxes >= THE_MINIMUM_OF_PROVISION)
 
     println(theSail)
 }
@@ -26,3 +20,5 @@ fun main() {
 const val THE_MINIMUM_OF_CREW = 55
 const val THE_MAXIMUM_OF_CREW = 70
 const val THE_MINIMUM_OF_PROVISION = 50
+const val IS_NOT_DAMAGED = true
+const val IS_GOOD_WEATHER = true
