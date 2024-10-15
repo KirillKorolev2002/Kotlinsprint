24f1000c187ad96b42d913d6ef46206ce03885f7
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-15 12:48:29 +0300

    KS 4-4 Last Changes

--- src/main/kotlin/Lesson 4/Task 4.kt
+++ src/main/kotlin/Lesson 4/Task 4.kt
@@ -3,14 +3,14 @@ package org.example.`Lesson 4`
 fun main() {
     val day = readln().toInt()
 
-    val Even = day % 2 == 0
+    val isEven = day % 2 == 0
 
     println(
         """
-        Упражнения для рук:       ${!Even}
-        Упражнения для ног:       ${Even}
-        Упражнения для спины:     ${Even}
-        Упражнения для пресса:    ${!Even}
+        Упражнения для рук:       ${!isEven}
+        Упражнения для ног:       ${isEven}
+        Упражнения для спины:     ${isEven}
+        Упражнения для пресса:    ${!isEven}
     """.trimIndent()
     )
 }
