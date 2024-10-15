b0d8ae1ae32dd3d5a179fe484646d050cac65c1a
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-15 15:48:13 +0300

    KS 6-5 Change package lesson 2 on 6+Reform.code

---
+++ src/main/kotlin/Lesson 6/Task 5.kt
@@ -0,0 +1,24 @@
+package org.example.`Lesson 6`
+
+fun main() {
+    var theTries = 3
+
+    while (theTries > 0) {
+        val theFirstInput = (1..9).random()
+        val theSecondInput = (1..9).random()
+        val theSumOfInputs = theFirstInput + theSecondInput
+
+        println("Сколько будет $theFirstInput + $theSecondInput?")
+        val theAnswer = readln().toInt()
+
+        if (theAnswer == theSumOfInputs) {
+            println("Добро пожаловать!")
+        } else {
+            theTries--
+            println("Неверно. Осталось попыток: $theTries")
+        }
+    }
+    if (theTries == 0) {
+        println("Доступ запрещен")
+    }
+}
