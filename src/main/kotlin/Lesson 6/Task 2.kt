8d0e7edd192b32d959705390f1dd270dce6270f9
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-15 14:02:51 +0300

    KS 6-2 Easy Counter

---
+++ src/main/kotlin/Lesson 6/Task 2.kt
@@ -0,0 +1,13 @@
+package org.example.`Lesson 6`
+
+fun main() {
+  val numberOfSeconds= readln().toInt()
+    var counterNumberOfSeconds=numberOfSeconds
+    do {
+        counterNumberOfSeconds--
+        println(counterNumberOfSeconds)
+        Thread.sleep(1000)
+    }
+    while (counterNumberOfSeconds > 0)
+        println("Прошло $numberOfSeconds секунд")
+}
