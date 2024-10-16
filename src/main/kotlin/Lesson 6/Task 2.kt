25723e4e74811cd25a6ff4292b2b4c28cc96437a
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-16 18:48:06 +0300

    KS 6-2 Changes++

--- src/main/kotlin/Lesson 6/Task 2.kt
+++ src/main/kotlin/Lesson 6/Task 2.kt
@@ -3,9 +3,8 @@ package org.example.`Lesson 6`
 fun main() {
     val numberOfSeconds = readln().toInt()
     var counterNumberOfSeconds = numberOfSeconds
-    do {
+        counterNumberOfSeconds > 0
         counterNumberOfSeconds--
         Thread.sleep(1000)
-    } while (counterNumberOfSeconds > 0)
     println("Прошло $numberOfSeconds секунд")
 }
