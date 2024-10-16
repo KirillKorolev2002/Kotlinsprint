906b28fcbe4bbaae9c4143277e70a887f3c1dcaa
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-16 22:44:37 +0300

    KS 7-3 Even Numbers

--- src/main/kotlin/Lesson 7/Task 3.kt
+++ src/main/kotlin/Lesson 7/Task 3.kt
@@ -1,2 +1,11 @@
 package `Lesson 7`
 
+fun main() {
+    val aRequest = readln().toInt()
+    val theNumber = aRequest.toUInt()
+    for (i in 0.toUInt() until theNumber) {
+        if ((i % 2u) == 0.toUInt()) {
+            print("$i\t")
+        }
+    }
+}
