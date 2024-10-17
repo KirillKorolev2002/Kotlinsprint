28b474d90719de262470a20e83209d845ff2568f
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-17 21:53:21 +0300

    KS 8-2 Changes

--- src/main/kotlin/Lesson 8/Task 2.kt
+++ src/main/kotlin/Lesson 8/Task 2.kt
@@ -3,8 +3,8 @@ package `Lesson 8`
 fun main() {
     val containsOfPizza = arrayOf("Ананас", "Колбаса", "Сыр", "Тесто", "Соус", "Специи")
     val theProduct = readln()
-    for (contains in containsOfPizza.indices) {
-            if (containsOfPizza[contains] == theProduct) {
+    for (contains in containsOfPizza) {
+            if (contains == theProduct) {
                     println("Ингредиент $theProduct в рецепте есть")
                     break
             }else
