914ac3dfbed1aad1100dff6991037886317b6128
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-17 10:56:14 +0300

    KS 7-5 Changes

--- src/main/kotlin/Lesson 7/Task 5.kt
+++ src/main/kotlin/Lesson 7/Task 5.kt
@@ -1,10 +1,12 @@
 package `Lesson 7`
+
 fun main() {
     val charRangeForPassword: CharRange = 'a'..'z'
     val intRangeForPassword: IntRange = 0..9
     val charRangeForPasswordBig: CharRange = 'A'..'Z'
     val thePassword = StringBuilder()
     val isLengthOfPassword = readln().toInt()
+
     if (isLengthOfPassword > MINIMUM_LENGTH) {
         for (i in 0 until isLengthOfPassword) {
             when ((0..2).random()) {
@@ -16,4 +18,5 @@ fun main() {
         println(thePassword)
     }
 }
+
 const val MINIMUM_LENGTH = 5
