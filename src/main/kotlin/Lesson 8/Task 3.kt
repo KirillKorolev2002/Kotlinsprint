a69a9dd69f58deba3ed63ea52f282743b6229037
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-17 22:04:24 +0300

    KS 8-3 Changes

--- src/main/kotlin/Lesson 8/Task 3.kt
+++ src/main/kotlin/Lesson 8/Task 3.kt
@@ -3,17 +3,7 @@ package `Lesson 8`
 fun main() {
     val containsOfPizza = arrayOf("Ананас", "Колбаса", "Сыр", "Тесто", "Соус", "Специи")
     val theProduct = readln()
-    if (theProduct == containsOfPizza[0]) {
-        println("В рецепте есть $theProduct")
-    } else if (theProduct == containsOfPizza[1]) {
-        println("В рецепте есть $theProduct")
-    } else if (theProduct == containsOfPizza[2]) {
-        println("В рецепте есть $theProduct")
-    } else if (theProduct == containsOfPizza[3]) {
-        println("В рецепте есть $theProduct")
-    } else if (theProduct == containsOfPizza[4]) {
-        println("В рецепте есть $theProduct")
-    } else if (theProduct == containsOfPizza[5]) {
+    if (theProduct in containsOfPizza[0].. containsOfPizza[5]) {
         println("В рецепте есть $theProduct")
     } else {
         println("В рецепте нет")
