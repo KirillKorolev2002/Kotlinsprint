ca365ad85049b1dbc0e4faf943a24cc023ea7826
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-18 07:54:39 +0300

    KS 9-1 ListOfIng.

---
+++ src/main/kotlin/Lesson 9/Task 9.kt
@@ -0,0 +1,10 @@
+package `Lesson 9`
+
+fun main() {
+    val listOfContains = listOf("Ананас", "Колбаса", "Сыр", "Тесто", "Соус", "Специи")
+    println(listOfContains)
+    println(listOfContains.forEach {
+        println(it)
+    })
+}
+
