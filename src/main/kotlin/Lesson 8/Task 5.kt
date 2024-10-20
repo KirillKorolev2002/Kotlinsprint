9005182ae21d1bc2331c9c8a16d02457f18c2428
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-20 16:14:58 +0300

    KS-8-5 Changes

--- src/main/kotlin/Lesson 8/Task 5.kt
+++ src/main/kotlin/Lesson 8/Task 5.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Введите количество ингредиентов:")
     val count = readln().toInt()
 
-    val ingredients = arrayOf("")
+    val ingredients = arrayOf(" ")
 
     for (i in 0 until count) {
         ingredients[i] = readln()
