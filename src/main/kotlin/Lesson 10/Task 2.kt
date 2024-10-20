b6acb5213958565abc08ca014282735c08d0e998
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-20 13:11:10 +0300

    KS-10-2

--- src/main/kotlin/Lesson 10/Task 2.kt
+++ src/main/kotlin/Lesson 10/Task 2.kt
@@ -1,2 +1,23 @@
 package `Lesson 10`
 
+fun main() {
+    getLogin()
+    getPassword()
+}
+
+fun getLogin() {
+    val introduceLogin = readln()
+    if (introduceLogin.length <= 4) {
+        println("Логин недостаточно длинный")
+    } else {
+        println(introduceLogin)
+    }
+}
+fun getPassword() {
+    val introducePassword = readln()
+    if (introducePassword.length <= 4) {
+        println("Пароль недостаточно длинный")
+    } else {
+        println(introducePassword)
+    }
+}
