33fc838888daf927744938db7b7a685526eb720c
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-20 19:12:07 +0300

    KS-11-1

---
+++ src/main/kotlin/Lesson 11/Task 1.kt
@@ -0,0 +1,37 @@
+package `Lesson 11`
+
+fun main() {
+
+    val metadata = User()
+    metadata.id = "@kirillkorolev2002"
+    metadata.login = 4040
+    metadata.password = "Losost"
+    metadata.mail = "kirillkorolev2002@mail.ru "
+    println(metadata.userToString())
+    val metadataold = oldUser()
+    metadataold.id = "@gleb.nikitin9696"
+    metadataold.login = 4040
+    metadataold.password = "Losost"
+    metadataold.mail = "gleb.nikitin.9696@mail.ru "
+    println(metadataold.userToString())
+}
+
+class User {
+    var id: String = " "
+    var login: Int = 40
+    var password: String = " "
+    var mail: String = " "
+    fun userToString(): String {
+        return "id: $id  login: $login password: $password  mail: $mail"
+    }
+}
+
+class oldUser {
+    var id: String = " "
+    var login: Int = 40
+    var password: String = " "
+    var mail: String = " "
+    fun userToString(): String {
+        return "id: $id  login: $login password: $password  mail: $mail"
+    }
+}
