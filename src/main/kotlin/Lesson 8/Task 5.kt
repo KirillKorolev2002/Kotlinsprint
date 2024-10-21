5116b605fcbe9290eb5982ddc460d6dae9b8d8c1
KirillKorolev2002 <155712734+KirillKorolev2002@users.noreply.github.com>
2024-10-21 12:33:52 +0300

    KS 8-5

--- src/main/kotlin/Lesson 8/Task 5.kt
+++ src/main/kotlin/Lesson 8/Task 5.kt
@@ -2,13 +2,7 @@ package `Lesson 8`
 
 fun main() {
     println("Введите количество ингредиентов:")
-    val count = readln().toInt()
-
-    val ingredients = arrayOf(" ")
-
-    for (i in 0 until count) {
-        ingredients[i] = readln()
-    }
+    val ingredients = Array(readln().toInt()) { readln() }
 
     println("Введенные ингредиенты: ${ingredients.joinToString(", ")}")
 }
